5c12ecec4bf601890516d0075acc0c4afff4668d
masaki-PC\masaki <mxm00011257@gmail.com>
2018-08-25 16:29:26 +0900

    Webシステム作成

--- MyEC/src/myec/RegistResult.java
+++ MyEC/src/myec/RegistResult.java
@@ -1,11 +1,14 @@
 package myec;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class RegistResult
@@ -13,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/RegistResult")
 public class RegistResult extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +29,9 @@ public class RegistResult extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		RequestDispatcher dispatcher = request.getRequestDispatcher(Controllor.REGIST_RESULT_PAGE);
+		dispatcher.forward(request, response);
 	}
 
 	/**
